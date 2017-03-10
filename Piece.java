4b002657da000638c5908ebd5ae56fd8cafef692
gcraig197 <gcraig197@gmail.com>
2017-03-10 12:41:20 +0000

    Piece enum

---
+++ Piece.java
@@ -0,0 +1,23 @@
+package game;
+
+public enum Piece {
+	ORANGE('B'),
+	DBLUE('B'),
+	LBLUE('B'),
+	PINK('B'),
+	YELLOW('B'),
+	RED('B'),
+	GREEN('B'),
+	BROWN('B'),
+	
+	ORANGE('W'),
+	DBLUE('W'),
+	LBLUE('W'),
+	PINK('W'),
+	YELLOW('W'),
+	RED('W'),
+	GREEN('W'),
+	BROWN('W');
+	
+	Private Char Team;
+}
